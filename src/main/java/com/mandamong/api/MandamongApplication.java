cb8e7f54992665119c0065eda881198e1a6bf9a3
digitpic <jkw5033@gmail.com>
2025-01-13 20:31:27 +0900

    chore: set up project

---
+++ src/main/java/com/mandamong/api/MandamongApplication.java
@@ -0,0 +1,11 @@
+package com.mandamong.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandamongApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MandamongApplication.class, args);
+    }
+}
