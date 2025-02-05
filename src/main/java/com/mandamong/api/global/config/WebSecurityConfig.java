4fed8202de2f3e0e796acd3d1a37faa88f26c34b
Seiyeon Joo <94334477+jjjooo-it@users.noreply.github.com>
2025-02-05 16:03:26 +0900

    feat(config): WebSecurityConfig 추가

--- src/main/java/com/mandamong/api/global/config/WebSecurityConfig.java
+++ src/main/java/com/mandamong/api/global/config/WebSecurityConfig.java
@@ -1,5 +1,70 @@
 package com.mandamong.api.global.config;
 
+import com.mandamong.api.member.filter.TokenAuthenticationFilter;
+import com.mandamong.api.member.repository.MemberRepository;
+import com.mandamong.api.member.util.TokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final MemberRepository memberRepository;
+
+    public WebSecurityConfig(TokenProvider tokenProvider, MemberRepository memberRepository) {
+        this.tokenProvider = tokenProvider;
+        this.memberRepository = memberRepository;
+    }
+
+    // 패스워드 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // TokenAuthenticationFilter를 Bean으로 등록
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기능 보안 구성
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/members").permitAll()
+                        .requestMatchers("/auth").permitAll()
+                        .requestMatchers("/oauth").permitAll()
+                        .requestMatchers("/token/refresh").permitAll()
+                        .requestMatchers("/api/health-check").permitAll()
+                        .anyRequest().authenticated()  // 나머지 엔드포인트는 인증 필요
+                )
+                .addFilterBefore(new TokenAuthenticationFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost");
+        configuration.addAllowedOrigin("http://localhost:3000");
+
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
 }
--- src/main/java/com/mandamong/api/member/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/mandamong/api/member/filter/TokenAuthenticationFilter.java
@@ -24,7 +24,8 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+            FilterChain filterChain
+    ) throws ServletException, IOException {
 
         String token = "";
         String authorizationHeader;
